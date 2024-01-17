8b3c7bb0c306e6f68b7fa3f421e1017a34078895
BlueButterflies <dsvk23020818@outlook.it>
2024-01-17 13:12:58 +0100

    added footer animation

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -27,7 +27,7 @@ function Footer() {
                 <h6 className="mt3">
                     <FaCopyright  className="copy"/>
                     <a href="https://bluebutterflies.github.io/portfolio/" target="_blank">
-                        {"2024 - Template developed by S/Z Siyana Zdravkova Web Developer"}
+                        2024 - Template developed by <span>S/Z Siyana Zdravkova Web Developer</span>
                     </a>
                 </h6>
         </Container>
